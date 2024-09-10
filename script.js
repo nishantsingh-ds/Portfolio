ef1f4d2e9ec8cb738974aff793c4f7c5896f5e00
Nishant Singh <166206623+NishantkSingh0@users.noreply.github.com>
2024-09-10 17:26:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ $(document).ready(function () {
             document.querySelector('#scroll-top').classList.remove('active');
         }
 
-        // scroll spy
         $('section').each(function () {
             let height = $(this).height();
             let offset = $(this).offset().top - 200;
@@ -29,7 +28,6 @@ $(document).ready(function () {
         });
     });
 
-    // smooth scrolling
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
@@ -37,7 +35,6 @@ $(document).ready(function () {
         }, 500, 'linear')
     });
 
-    // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
         emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
 
@@ -52,7 +49,6 @@ $(document).ready(function () {
             });
         event.preventDefault();
     });
-    // <!-- emailjs to mail contact form data -->
 
 });
 
@@ -69,7 +65,6 @@ document.addEventListener('visibilitychange',
     });
 
 
-// <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
     strings: ["2+ year of Experience on Machine learning","worked on Vision Transformer based object classification Models","worked on RAG models","worked on Transformer large language Models","worked on CNN based Object Segmentation Models", "worked on Transformer based Translation Models","worked on RNN based Text Generation Model"],
     loop: true,
@@ -77,7 +72,7 @@ var typed = new Typed(".typing-text", {
     backSpeed: 30,
     backDelay: 500,
 });
-// <!-- typed js effect ends -->
+
 
 async function fetchData(type = "skills") {
     let response
@@ -127,13 +122,11 @@ function showProjects(projects) {
     });
     projectsContainer.innerHTML = projectHTML;
 
-    // <!-- tilt js effect starts -->
+
     VanillaTilt.init(document.querySelectorAll(".tilt"), {
         max: 15,
     });
-    // <!-- tilt js effect ends -->
 
-    /* ===== SCROLL REVEAL ANIMATION ===== */
     const srtop = ScrollReveal({
         origin: 'top',
         distance: '80px',
@@ -141,7 +134,7 @@ function showProjects(projects) {
         reset: true
     });
 
-    /* SCROLL PROJECTS */
+
     srtop.reveal('.work .box', { interval: 200 });
 
 }
@@ -154,24 +147,7 @@ fetchData("projects").then(data => {
     showProjects(data);
 });
 
-// <!-- tilt js effect starts -->
-/*VanillaTilt.init(document.querySelectorAll(".tilt"), {
-    max: 15,
-});*/
-// <!-- tilt js effect ends -->
 
-
-// pre loader start
-// function loader() {
-//     document.querySelector('.loader-container').classList.add('fade-out');
-// }
-// function fadeOut() {
-//     setInterval(loader, 500);
-// }
-// window.onload = fadeOut;
-// pre loader end
-
-// disable developer mode
 document.onkeydown = function (e) {
     if (e.keyCode == 123) {
         return false;
@@ -190,20 +166,7 @@ document.onkeydown = function (e) {
     }
 }
 
-// // Start of Tawk.to Live Chat
-// var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-// (function () {
-//     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-//     s1.async = true;
-//     s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
-//     s1.charset = 'UTF-8';
-//     s1.setAttribute('crossorigin', '*');
-//     s0.parentNode.insertBefore(s1, s0);
-// })();
-// // End of Tawk.to Live Chat
 
-
-/* ===== SCROLL REVEAL ANIMATION ===== */
 const srtop = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -211,7 +174,6 @@ const srtop = ScrollReveal({
     reset: true
 });
 
-/* SCROLL HOME */
 srtop.reveal('.home .content h3', { delay: 200 });
 srtop.reveal('.home .content p', { delay: 200 });
 srtop.reveal('.home .content .btn', { delay: 200 });
@@ -224,7 +186,6 @@ srtop.reveal('.home .telegram', { interval: 600 });
 srtop.reveal('.home .instagram', { interval: 600 });
 srtop.reveal('.home .dev', { interval: 600 });
 
-/* SCROLL ABOUT */
 srtop.reveal('.about .content h3', { delay: 200 });
 srtop.reveal('.about .content .tag', { delay: 200 });
 srtop.reveal('.about .content p', { delay: 200 });
@@ -232,20 +193,15 @@ srtop.reveal('.about .content .box-container', { delay: 200 });
 srtop.reveal('.about .content .resumebtn', { delay: 200 });
 
 
-/* SCROLL SKILLS */
 srtop.reveal('.skills .container', { interval: 200 });
 srtop.reveal('.skills .container .bar', { delay: 400 });
 
-/* SCROLL EDUCATION */
 srtop.reveal('.education .box', { interval: 200 });
 
-/* SCROLL PROJECTS */
 srtop.reveal('.work .box', { interval: 200 });
 
-/* SCROLL EXPERIENCE */
 srtop.reveal('.experience .timeline', { delay: 400 });
 srtop.reveal('.experience .timeline .container', { interval: 400 });
 
-/* SCROLL CONTACT */
 srtop.reveal('.contact .container', { delay: 400 });
 srtop.reveal('.contact .container .form-group', { delay: 400 });
